6f95772ec12e3de0ea14ad1ce455095455b31ec3
harurusukooni <gragrapiyoko@gmail.com>
2026-01-18 23:45:51 +0900

    再帰的な関数を利用してフィボナッチ数列を実装しようの課題完了

--- pythondev/prognum.py
+++ pythondev/prognum.py
@@ -1,5 +1,6 @@
-def ratio(n):
-    if n < 2:
+def fibo(n):
+    if n == 0 :
+        return 0
+    if n <= 2 :
         return 1
-    
-    return n * factorial(n - 1)
+    return fibo(n-2) + fibo(n-1)
